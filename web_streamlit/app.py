61e527029c5274063df19b7f5c0a999ce6748337
Alejandro Núñez Arroyo <50973610+AlejandroNunezArroyo@users.noreply.github.com>
2022-11-18 04:49:21 -0400

    Update app.py

--- web_streamlit/app.py
+++ web_streamlit/app.py
@@ -20,8 +20,8 @@ def get_model():
 @st.cache(allow_output_mutation=True)
 def get_extra():
     stop_words = stopwords.words('spanish')
-    image = Image.open('./image.jpg')
-    data_test = pd.read_csv('./test_df.csv')
+    image = Image.open('./file/image.jpg')
+    data_test = pd.read_csv('./file/test_df.csv')
     df = data_test[['clean_token_head_con', 'label']]
     return stop_words, image, df
 
