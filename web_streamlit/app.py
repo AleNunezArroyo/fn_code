16d64eff099d0a9cc3260c8185f0a46d919da260
Alejandro Núñez Arroyo <50973610+AlejandroNunezArroyo@users.noreply.github.com>
2022-11-19 05:44:20 -0400

    edit

--- web_streamlit/app.py
+++ web_streamlit/app.py
@@ -37,33 +37,17 @@ tokenizer,model = get_model()
 stop_words, image, df = get_extra()
 
 st.image(image, caption='Estudiante: Alejandro Núñez Arroyo | Tutor: Ing. Guillermo Sahonero')
+col1, col2 = st.columns(2)
+col1.metric("Izquierda probabilidad noticia verdadero", "0")
+col2.metric("Derecha probabilidad noticia falso: ", "1")
 
-# st.title("Ejemplos de texto:")
-# option = st.selectbox(
-#     'Ejemplos de datos de prueba:',
-#     ('Dirigentes del transporte cruceño alentaron a salir con palos a desbloquear | Falso', 
-#      'UTO producirá dióxido de cloro y su Rector denuncia que los insumos ya subieron | Verdadero'))
 
-user_input = st.text_area('Ingresar texto para revisión', value="Escribe aquí el extracto a verificar o prueba un ejemplo...", key=1)
+user_input = st.text_area('Ingresar texto para revisión')
 pre_pro = st.radio(
     "Seleccione el filtrado:",
     ('Con preprocesamiento', 'Sin preprocesamiento'))
+st.dataframe(df)
 button = st.button("Analizar")
-
-st.sidebar.title("Información adicional")
-st.sidebar.info('Descarga la guía de usuario:')
-link='[ENLACE](https://miro.medium.com/max/1200/1*h26H6n9C9MzLmUtI7-EAcA.gif)'
-st.sidebar.markdown(link,unsafe_allow_html=True)
-st.sidebar.header('Falso: ')
-st.sidebar.markdown('Vacunas genéticas provocan daño al ser humano')
-st.sidebar.markdown('Red Uno publicó que Iván Arias y Eva Copa hayan realizado una alianza')
-
-st.sidebar.header('Verdadero: ')
-st.sidebar.markdown('Tarija Cuatro menores fugaron del centro Oasis')
-st.sidebar.markdown('Alanez exclama que tiene los pantalones bien amarrados y sus cocaleros abren marcha pacífica')
-
-st.sidebar.header('No hace inferencia: ')
-st.sidebar.markdown('Dirigentes del transporte cruceño alentaron a salir con palos a desbloquear')
 
 def joined_data(text):
     try:
@@ -98,8 +82,8 @@ if user_input and button:
     else:
         user_input = user_input
     
-    st.write("Texto de entrada al sistema: ",user_input, key="3")
-    st.write("Longitud: ",len(user_input.split(' ')), key="4")
+    st.write("Texto de entrada al sistema: ",user_input)
+    st.write("Longitud: ",len(user_input.split(' ')))
     encoded_review = tokenizer.encode_plus(
         user_input,
         max_length=MAX_SEQ_LEN,
@@ -115,22 +99,7 @@ if user_input and button:
     model_out = model(input_ids,attention_mask)
     all_logits = torch.nn.functional.log_softmax(model_out.logits, dim=1)
     probs = F.softmax(all_logits, dim=1)
-    p_ = torch.squeeze(probs)
-    p_ = p_.tolist()
-    col1_, col2_ = st.columns(2)
-    if (round(p_[0], 5) > 0.9057):
-        with col1_:
-            st.success('Probalidad de Verdadero: ', icon="✅")
-        with col2_:
-            st.header((round(p_[0], 3)*100), ' %')
-        st.info('Predicción realizada con éxito')
-        
-    elif (round(p_[1], 5) > 0.8809):
-        with col1_:
-            st.error('El texto de entrada necesita atención. Probalidad de falso: ', icon="🚨")
-        with col2_:
-            st.header((round(p_[1], 3)*100), ' %')
-        st.info('Predicción realizada con éxito')
-    else:
-        st.info('Intenta con otra noticia')
-        st.write(round(p_[0], 5), (round(p_[0], 5)))
+    st.write("Logits: ",probs)
+    
+
+
